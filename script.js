09ef7beb1adbb02491476a5fe8dd7fcd21c759b5
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2024-09-23 16:06:15 -0300

    del comments

--- script.js
+++
@@ -1,6 +0,0 @@
-
-
-
-//Achei que se eu baixasse aparecia pra mim, mas i stylesheet não aparecia, do css não apareceu. to abrindo no navegador
-//só pra constar... tu fez o reset lá?, vou mostrar
-//apareceu nada dele? te envio separado qualquer coisa, quer? que reset?
